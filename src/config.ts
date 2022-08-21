0a2c3d6ae9a6da78d25adb3b3e0694643037894e
quatre29 <quatre29@gmail.com>
2022-08-21 17:17:16 +0100

    Heroku

--- src/config.ts
+++ src/config.ts
@@ -17,6 +17,7 @@ switch (process.env.NODE_ENV) {
     break;
   case "production":
     console.log("Environment is production");
+    break;
   default:
     throw new Error(`NODE_ENV ${process.env.NODE_ENV} is not handled!`);
 }
